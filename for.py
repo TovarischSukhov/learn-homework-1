38418fb561dba0a7bc7844e6f886cd596d70bc0e
axl686 <malinalexey@gmail.com>
2019-05-22 15:58:06 +0300

    task

--- for.py
+++ for.py
@@ -9,13 +9,25 @@
 * Посчитать и вывести средний балл по всей школе.
 * Посчитать и вывести средний балл по каждому классу.
 """
+classes = [
+  {'school_class': '4a', 'scores': [3,4,4,5,2]},
+  {'school_class': '4b', 'scores': [3,5,5,3,1]},
+  {'school_class': '4c', 'scores': [4,2,3,3,1]},
+  {'school_class': '4d', 'scores': [5,4,4,5,5]},
+  ]
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
+def main(classes):
+  avgScore = []
+  
+  for dicts in classes:
+    avgScore.append(sum(dicts['scores']) / float(len(dicts['scores'])))
+    print(
+      dicts['school_class'],
+      ':',
+      sum(dicts['scores']) / float(len(dicts['scores']))
+    )
+
+  print('All school avarage score:', sum(avgScore)/len(avgScore))
+
 if __name__ == "__main__":
     main()
